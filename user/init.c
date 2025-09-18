7b3a9ff39f95693e3858e5801a0ff505771d844e
youya199 <94166804+youya199@users.noreply.github.com>
2025-09-18 14:29:47 +0200

    Update init.c

--- user/init.c
+++ user/init.c
@@ -31,7 +31,7 @@ main(void)
       exit(1);
     }
     if(pid == 0){
-      exec("sh", argv);
+      exec("sh", argv);// here we boot into the shell terminal of xv6
       printf("init: exec sh failed\n");
       exit(1);
     }
